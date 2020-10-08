8d1ee31ecdd18d037d94b1218812bd73ae9ec3e4
Martin <martinandrovich@gmail.com>
2020-10-08 22:20:55 +0200

    add dual line follow PID, intersection find and square test

--- aibot/nav.py
+++ aibot/nav.py
@@ -2,26 +2,44 @@
 
 from aibot import hw
 from time import sleep
-	
+
+DIST_FOLLOW_LINE = 500
+DIST_FOLLOW_GYRO = 80
+
+is_cal = False
 
 def forward(speed = 30):
-        # calibrate the gyroscope
-        hw.gs.calibrate()
-        # drive forward with the color sensor
-        hw.forward_light_for(500,"ticks")
-        # align with the gyroscope
-        hw.motors.turn_degrees(SpeedPercent(5), -hw.gs.angle)
-        # drive forward with the gyroscope
-        hw.forward_gyro_for(112,"ticks")
-        # align with the gyroscope
-        hw.motors.turn_degrees(SpeedPercent(5), -hw.gs.angle)
+
+	# calibrate the gyroscope
+	hw.motors.wait_until_not_moving()
+	
+	global is_cal
+	if not is_cal:
+		print("calibrating...")
+		hw.gs.calibrate()
+		is_cal = True
+
+	hw.gs.reset()
+
+	# drive forward with the color sensor
+	hw.motors.follow_line_for(DIST_FOLLOW_LINE, speed) # ticks
+
+	# drive forward with the gyroscope (angle = 0)
+	hw.motors.follow_gyro_for(DIST_FOLLOW_GYRO, 0, speed)
+	
+	# align with the gyroscope
+	hw.motors.turn_degrees(hw.SpeedPercent(5), -hw.gs.angle)
 
 def turn(dir):
-    angle = 90 * (-1 if dir == "left" else 1)
-    mtank.turn_degrees(SpeedPercent(5),angle)
+
+	angle = 90 * (-1 if dir == "left" else 1)
+	hw.motors.turn_degrees(hw.SpeedPercent(5), angle)
 
 def drive_square(speed = 30):
-    while True:
-        forward()
-        turn("right")
+
+	while True:
+		forward()
+		sleep(1)
+		turn("right")
+		sleep(1)
 
